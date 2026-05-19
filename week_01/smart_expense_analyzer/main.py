8117e3eabb59c0a5f7bef8ffe031858f3501f8d7
Hazel Garcia <ana.garcia.sanch@gmail.com>
2026-05-18 19:43:56 -0600

    Added first expense tracking logic

--- week_01/smart_expense_analyzer/main.py
+++ week_01/smart_expense_analyzer/main.py
@@ -1,2 +1,16 @@
-print("Hello Zede, engine on")
-print("Prueba 1 de commit")
+print("Smart Expense Analyzer")
+
+expenses = [
+    {"category": "food", "amount": 250},
+    {"category": "transport", "amount": 100},
+    {"category": "games", "amount": 500}
+]
+
+total = 0
+
+for expense in expenses:
+    print(expense)
+
+    total = total + expense["amount"]
+
+print("Total expenses:", total)
