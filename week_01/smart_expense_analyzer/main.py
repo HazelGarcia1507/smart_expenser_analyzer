9f4bb4c6caac1de8ca0124879dcb1eeb608b70e0
Hazel Garcia <ana.garcia.sanch@gmail.com>
2026-05-19 01:27:48 -0600

    Added expense loop and improved user input handling

--- week_01/smart_expense_analyzer/main.py
+++ week_01/smart_expense_analyzer/main.py
@@ -1,16 +1,35 @@
 print("Smart Expense Analyzer")
+   
 
 expenses = [
-    {"category": "food", "amount": 250},
-    {"category": "transport", "amount": 100},
-    {"category": "games", "amount": 500}
-]
+        {"category": "food", "amount": 250},
+        {"category": "transport", "amount": 100},
+        {"category": "games", "amount": 500},
+    ]
 
-total = 0
+continue_choice = 'yes'
+while continue_choice == "yes": 
 
-for expense in expenses:
-    print(expense)
+    new_category = input("Enter expense category: ")
+    new_amount = float(input("Enter expense amount: "))
 
-    total = total + expense["amount"]
 
-print("Total expenses:", total)
+    expenses.append(
+            {"category": new_category, "amount": new_amount}
+        )
+
+    total = 0
+
+    for expense in expenses:
+        print(expense)
+
+        total = total + expense["amount"]
+
+    print("Total expenses:", total) 
+
+
+    continue_choice = input("Add another expense? yes/no: ").lower()
+
+
+# Future improvement:
+# Validate yes/no input
